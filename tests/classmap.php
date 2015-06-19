8da43bf3295892bc776b844b15abed8edbcc0649
elazar <tobias382@gmail.com>
2015-06-19 14:50:46 -0500

    Removed tests/classmap.php
    Previously, Composer excluded files with filenames ending in
    "Test.php" from those it included in generated classmaps. This was
    changed in this commit:
    
    https://github.com/composer/composer/commit/8e9659bd8317ad3f623c4d17940380443cf8772c
    
    Because of this change, Composer classmaps can now be used together
    with ClassmapFileObjectTestLoader. As such, the existing manually
    maintained classmap for Phantestic's test suite is being removed
    and its runner modified to use Composer's classmap.

--- tests/classmap.php
+++
@@ -1,7 +0,0 @@
-<?php
-$classmap = require __DIR__ . '/../vendor/composer/autoload_classmap.php';
-$classmap += [
-    'Phantestic\\Tests\\TestCase\\TestCaseTest' => __DIR__ . '/TestCase/TestCaseTest.php',
-    'Phantestic\\Tests\\TestHandler\\CliReporterTest' => __DIR__ . '/TestHandler/CliReporterTest.php',
-];
-return $classmap;
--- tests/runner.php
+++ tests/runner.php
@@ -1,7 +1,7 @@
 #!/usr/bin/env php
 <?php
 require __DIR__ . '/../vendor/autoload.php';
-$classmap_path = __DIR__ . '/classmap.php';
+$classmap_path = __DIR__ . '/../vendor/composer/autoload_classmap.php';
 $loader = new \Phantestic\TestLoader\ClassmapFileObjectTestLoader($classmap_path);
 $handlers = [ new \Phantestic\TestHandler\CliReporter ];
 $runner = new \Phantestic\TestRunner\LocalTestRunner($loader, $handlers);
